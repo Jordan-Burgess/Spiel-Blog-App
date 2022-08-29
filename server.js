198fadd109f45dde53a5d4db9db716d8976446c3
chloewigsten <chloewigsten@gmail.com>
2022-08-29 12:09:40 -0400

    add controller connection to server.js

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const methodOverride = require('method-override');
 const { PromiseProvider } = require('mongoose');
+const session = require('express-session')
+const MongoStore = require('connect-mongo')
 
 //Make sure this is the right file path
 require('./config/db.connection');
 
-const blogsController = require('./controllers/blog_controller')
-const commentsController = require('./controllers/comments_controller')
+//Import Controllers 
+const controllers = require('./controllers')
 
 
 require('dotenv').config()
@@ -14,11 +16,26 @@ const app = express();
 const PORT = process.env.PORT
 app.set('view engine', 'ejs')
 
+//User Sessions
+app.use(
+    session[{
+        store: MongoStore.create({ mongoUrl: process.env.MONGO_URI}), 
+        secret: 'super secret', 
+        resave: false, 
+        saveUninitialized: false, 
+        cookie: {
+            maxAge: 1000 * 60 * 60 * 24 * 7,
+        }
+    }]
+)
+
+
 //Middleware
 app.use(express.static('public'))
 app.use(methodOverride('_method'));
-app.use('/blogs', blogsController);
-app.use('/comments', commentsController);
+app.use('/blogs', controllers.blogs);
+app.use('/comments', controllers.comments);
+app.use('', controllers.users)
 
 //Home Route
 app.get('/', (req,res) => {
