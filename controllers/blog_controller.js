7313b0ec02b4474e5e38122d04f2ad31e2be7fe4
chloewigsten <chloewigsten@gmail.com>
2022-08-29 10:50:19 -0400

    add show, update, delete, and edit rroutes to controller page

--- controllers/blog_controller.js
+++ controllers/blog_controller.js
@@ -17,7 +17,7 @@ router.post('/', async (req,res) => {
     const createdBlog = req.body;
 
     try {
-        const newBlog = await.db.Blogs.create(createdBlog);
+        const newBlog = await db.Blogs.create(createdBlog);
 
         console.log(newBlog);
 
@@ -28,3 +28,62 @@ router.post('/', async (req,res) => {
     }
 });
 
+//Show Route
+router.get('/:blogIndex', async (req,res) => {
+    try {
+        const foundBlog = await db.Blogs.findById(req.params.blogIndex)
+        res.render('show.ejs', {blog:foundBlog, id: foundBlog._id})
+    } catch(err) {
+        console.log(err)
+        res.redirect('/404')
+    }
+});
+
+//Index Route
+router.get('/', async (req,res) => {
+    try {
+        const allBlogs = await db.Blogs.find()
+        const context =  {blogs: allBlogs};
+        console.log(allBlogs)
+        res.render('index.ejs', context)
+    } catch (err) {
+        console.log(err)
+        res.redirect('/404')
+    }
+});
+
+//Destroy/Delete
+router.delete('./productId', async (req,res) => {
+    try {
+        const foundBlog = await db.Blogs.findByIdAndDelete(req.params.blogId)
+        console.log(foundBlog)
+        return res.redirect('/blogs');
+    } catch(err) {
+        console.log(err)
+        res.redirect('/404')
+    }
+});
+
+//Edit Route
+router.get('./blogId/edit', async (req,res) => {
+    try {
+        const foundBlog = await db.Blogs.findById(req.params.blogId)
+        console.log(foundBlog)
+        res.render('edit.ejs', { blog: foundBlog, id: foundBlog._id})
+    } catch(err) {
+        console.log(err)
+        res.redirect('/404')
+    }
+});
+
+//Update Route 
+router.put('./blogId', async (req,res) => {
+    try{
+        const updatedBlog = req.body;
+        await db.Blogs.findByIdAndUpdate(req.params.blogId, updatedBlog, {new:true})
+        res.redirect(`/blogs/${req.params.blogId}`);
+    } catch(err) {
+        console.log(err)
+        res.redirect('/404')
+    }
+});
