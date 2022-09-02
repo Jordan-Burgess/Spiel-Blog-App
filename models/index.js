4b319b09b2b86a5d49223960d25d13e7afee498e
chloewigsten <chloewigsten@gmail.com>
2022-09-02 10:08:34 -0400

    add heruko attempt 1

--- models/index.js
+++ models/index.js
@@ -6,3 +6,5 @@ module.exports = {
     User: require('./User'),
     Genre: require('./Genre')
 }
+
+mongoose.connect( process.env.MONGODB_URI || "mongodb+srv://spiel:spiel@cluster0.rg5icpe.mongodb.net/?retryWrites=true&w=majority" );
--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const genresController = require("./controllers/genres_controller")
 
 require('dotenv').config()
 const app = express();
-const PORT = process.env.PORT;
+app.listen(process.env.PORT || 4000);
 app.set('view engine', 'ejs')
 
 //User Sessions
