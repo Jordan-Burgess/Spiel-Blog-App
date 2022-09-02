ea27080a745e1f730b7157c73388b93c4f57e6db
Jordan-Burgess <alfredjordan93@gmail.com>
2022-09-02 16:13:23 +0000

    Edit new.ejs

--- models/genre_models.js
+++ models/genre_models.js
@@ -1,10 +1,10 @@
 const genres =  [
 	{
-			genre: "  Travel  ",
+			genre: "Travel",
 			img: "https://images.unsplash.com/photo-1661997011608-a8bc1a4da810?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1476&q=80",
 	}, 
 	{
-			genre: "  Food  ",
+			genre: "Food",
 			img: "https://images.unsplash.com/photo-1606787366850-de6330128bfc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
 	},
 	{
@@ -26,9 +26,9 @@ const genres =  [
 
 // async function reloadData() {
 // 	try {
-// 		let deleted = await db.Genres.deleteMany({});
+// 		let deleted = await db.Genre.deleteMany({});
 // 		console.log(deleted)
-// 		let reloading = await db.Genres.insertMany(genres);
+// 		let reloading = await db.Genre.insertMany(genres);
 // 		console.log(reloading)
 // 	} catch (err) {
 // 		console.log(err);
